8e7fbb86a16adcefe6604406df891ac03ed944bc
Danila <gendel2403@gmail.com>
2023-02-20 13:54:47 +0100

    slightly cleaner but found a segfault

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 17:53:20 by sbritani          #+#    #+#             */
-/*   Updated: 2023/02/20 13:28:54 by dhendzel         ###   ########.fr       */
+/*   Updated: 2023/02/20 13:52:20 by dhendzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -295,6 +295,58 @@ int	check_angulars(char **splitted_input)
 	return (1);
 }
 
+void clear_splits(char **splitted_input, char ***resplitted_input)
+{
+	ft_split_clear(splitted_input);
+	free_resplitted(resplitted_input);
+}
+
+int check_basic_commands(char **splitted_input, char ***resplitted_input, t_settings *settings)
+{
+	if (strings_equal(splitted_input[0], "exit"))
+		return (1);
+	if (!splitted_input[0])
+		return (1);
+	if (strings_equal(splitted_input[0], "echo\0"))
+		return (1);
+	if (strings_equal(splitted_input[0], "pwd\0"))
+		return (1);
+	if (strings_equal(splitted_input[0], "cd\0"))
+		return (1);
+	if (strings_equal(splitted_input[0], "env\0"))
+		return (1);
+	if (strings_equal(splitted_input[0], "unset\0"))
+		return (1);
+	if (strings_equal(splitted_input[0], "export\0"))
+		return (1);
+	if (ft_strchr(splitted_input[0], '='))
+		return (1);
+	return(0);
+}
+
+int basic_commands(char **splitted_input, char ***resplitted_input, t_settings *settings)
+{
+	if (strings_equal(splitted_input[0], "exit"))
+		return (clear_splits(splitted_input, resplitted_input), 0);
+	else if (!splitted_input[0])
+		return (clear_splits(splitted_input, resplitted_input), 1);
+	else if (strings_equal(splitted_input[0], "echo\0"))
+		settings->last_exit_status = echo(splitted_input + 1);
+	else if (strings_equal(splitted_input[0], "pwd\0"))
+		settings->last_exit_status = pwd(splitted_input);
+	else if (strings_equal(splitted_input[0], "cd\0"))
+		settings->last_exit_status = cd(splitted_input, settings);
+	else if (strings_equal(splitted_input[0], "env\0"))
+		settings->last_exit_status = env(splitted_input, settings);
+	else if (strings_equal(splitted_input[0], "unset\0"))
+		settings->last_exit_status = unset(splitted_input, settings);
+	else if (strings_equal(splitted_input[0], "export\0"))
+		settings->last_exit_status = export(splitted_input, settings);
+	else if (ft_strchr(splitted_input[0], '='))
+		settings->last_exit_status = deal_with_equal_sign(splitted_input, settings);
+	return (clear_splits(splitted_input, resplitted_input), 1);
+}
+
 int parse_input(char *input, t_settings *settings,char **envp)
 {
 	char **splitted_input;
@@ -310,67 +362,8 @@ int parse_input(char *input, t_settings *settings,char **envp)
 	resplitted_input = resplit(splitted_input);
 	if (count_resplitted(resplitted_input) == 1)
 	{
-		if (strings_equal(splitted_input[0], "exit"))
-		{
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (0);
-		}
-		if (!splitted_input[0])
-		{
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "echo\0"))
-		{
-			settings->last_exit_status = echo(splitted_input + 1);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "pwd\0"))
-		{
-			settings->last_exit_status = pwd(splitted_input);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "cd\0"))
-		{
-			settings->last_exit_status = cd(splitted_input, settings);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "env\0"))
-		{
-			settings->last_exit_status = env(splitted_input, settings);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "unset\0"))
-		{
-			settings->last_exit_status = unset(splitted_input, settings);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (strings_equal(splitted_input[0], "export\0"))
-		{
-			settings->last_exit_status = export(splitted_input, settings);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
-		if (ft_strchr(splitted_input[0], '='))
-		{
-			settings->last_exit_status = deal_with_equal_sign(splitted_input, settings);
-			ft_split_clear(splitted_input);
-			free_resplitted(resplitted_input);
-			return (1);
-		}
+		if((check_basic_commands(splitted_input, resplitted_input, settings)))
+			return(basic_commands(splitted_input, resplitted_input, settings));
 		else 
 		{
 			t_pipex pipex;
